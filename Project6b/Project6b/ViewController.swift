77a038b09ba2bc4808ece2620b11be9470cff674
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-01-31 09:32:47 -0300

    ATT layout para usar anchors

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -52,28 +52,49 @@ class ViewController: UIViewController {
         
         let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
         
-        for label in viewsDictionary.keys {
-            /**
-             The H: part means that we're defining a horizontal layout
-             The pipe symbol, |, means "the edge of the view." We're adding these constraints to the main view inside our view controller, so this effectively means "the edge of the view controller"
-             */
-            
-            // Horizontal layout
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
-        }
+        
+        //MARK: VFL code
+//        for label in viewsDictionary.keys {
+//            /**
+//             The H: part means that we're defining a horizontal layout
+//             The pipe symbol, |, means "the edge of the view." We're adding these constraints to the main view inside our view controller, so this effectively means "the edge of the view controller"
+//             */
+//
+//            // Horizontal layout
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
 
         // Vertical layout
         // the - symbol means "space". It's 10 points by default
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
 
         /**
          Espacos dentro do parentese apos o nome do label, nesse caso, estamos usando o dicionario criado antes do viewDidLoad
          O que vai dentro do () é colocado ali como metrics
          O @ significa prioridade, qualquer coisa menor que 1000 (o default) ser opcional e o auto-layout tentara ajustar o maximo que da caso nao seja possivel cumprir
          */
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
+        
 
 
+        //MARK: Anchors
+        var previous: UILabel?
+
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if let previous = previous {
+                // espaco entre as labels
+                // Pega o bottomAnchor da anterior e soma 10, dando assim um efeito de margin
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                // Espaca da safe area
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            previous = label
+        }
 
     }
 
